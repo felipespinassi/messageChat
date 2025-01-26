077821b793eaa05a4b56ed10ee8713df7da69535
Felipe Augusto <felipe_spinassi@hotmail.com>
2025-01-25 23:09:41 -0300

    feat: change route path

--- app/(authenticated)/(stack)/_layout.tsx
+++ app/(authenticated)/(stack)/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 export default function StackLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(stack)/chat/[id]" />
+      <Stack.Screen name="chat/[id]" />
     </Stack>
   );
 }
