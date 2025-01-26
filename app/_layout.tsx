037b1f26142b17b78905a7245eff5c6ebb1b5823
Felipe Augusto <felipe_spinassi@hotmail.com>
2025-01-25 23:17:27 -0300

    feat: add protected routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,30 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import "../styles/global.css";
 import { socket, WebSocketContext } from "@/context/webSocketContext";
+import { useEffect, useState } from "react";
+import { getAccess_token } from "@/storage/getAccess_token";
 
 export default function RootLayout() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const token = await getAccess_token();
+      setIsAuthenticated(!!token);
+    };
+    checkAuth();
+  }, []);
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.replace("/login");
+    } else {
+      router.replace("/chat");
+    }
+  }, [isAuthenticated]);
+
   return (
     <GluestackUIProvider mode="light">
       <WebSocketContext.Provider value={socket}>
